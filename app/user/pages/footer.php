acf7ef4c30b057494ebd2832d41b5562df2f55b4
Rama200404 <160811161+Rama200404@users.noreply.github.com>
2024-11-19 14:11:46 +0700

    Add files via upload

---
+++ app/user/pages/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 1.1
+    </div>
+    <strong>Hak Cipta &copy; <?= date('Y'); ?>. Team</strong> Wawan Koswara | Repost by <a href='https://www.instagram.com/wawankoswara24/' title='www.instagram.com' target='_blank'>instagram</a>
+    
+</footer>
